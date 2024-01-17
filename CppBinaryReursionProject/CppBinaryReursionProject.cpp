7e1e2f6581c20ad3c6583339c0161843b35b3a82
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-01-17 21:20:17 +0300

    Добавьте файлы проекта.

---
+++ CppBinaryReursionProject/CppBinaryReursionProject.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+double PowerLine(double x, int n);
+double PowerBinary(double x, int n);
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+double PowerLine(double x, int n)
+{
+    double result(1);
+    for (int i{}; i < n; i++)
+        result *= x;
+    return result;
+}
+
+double PowerBinary(double x, int n)
+{
+    double result(1);
+
+    while (n)
+    {
+        /*if (n & 1)
+        {
+            result *= x;
+            n--;
+        }
+        else
+        {
+            x *= x;
+            n /= 2;
+        }*/
+
+        if (n & 1)
+            result *= x;
+        x *= x;
+        n /= 2;
+    }
+
+    return result;
+}
